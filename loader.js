b0917a4dee0908df7132ecfc90169a977c678edb
Orghub-systems <orghubsystems@gmail.com>
2026-09-16 15:25:44 +0200

    Add RCP application loader

---
+++ loader.js
@@ -0,0 +1,19 @@
+(() => {
+  const parts = [
+    './app.part01.txt',
+    './app.part02.txt',
+    './app.part03.txt',
+    './app.part04.txt',
+    './app.part05.txt',
+    './app.part06.txt',
+  ];
+  Promise.all(parts.map(p => fetch(p, { cache: 'no-store' }).then(r => {
+    if (!r.ok) throw new Error(`Nie udało się pobrać ${p}`);
+    return r.text();
+  }))).then(chunks => {
+    (0, eval)(chunks.join(''));
+  }).catch(err => {
+    console.error(err);
+    document.getElementById('app').innerHTML = '<div class="shell"><div class="card notice-error">Nie udało się uruchomić aplikacji.</div></div>';
+  });
+})();
