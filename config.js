0c27ad62e1dcb097c95ecb5c39cd8019c8854937
Orghub-systems <orghubsystems@gmail.com>
2026-09-16 15:25:28 +0200

    Add RCP Supabase client config

---
+++ config.js
@@ -0,0 +1,6 @@
+window.RCP_CONFIG = Object.freeze({
+  supabaseUrl: 'https://nouanhglqawrmvhlzrez.supabase.co',
+  supabasePublishableKey: 'sb_publishable_gebqGvoF-vueb91wBH2Jhw_NCAzhkux',
+  timezone: 'Europe/Warsaw',
+  appName: 'RCP'
+});
