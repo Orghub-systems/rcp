b3acac981ae42b485ca2297ec9ee443e50d0e4a6
Orghub-systems <orghubsystems@gmail.com>
2026-09-16 15:26:05 +0200

    Add service worker

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE = 'rcp-shell-v3';
+const SHELL = [
+  './',
+  './index.html',
+  './styles.css',
+  './loader.js',
+  './app.part01.txt',
+  './app.part02.txt',
+  './app.part03.txt',
+  './app.part04.txt',
+  './app.part05.txt',
+  './app.part06.txt',
+  './config.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(r => r || caches.match('./index.html')))
+  );
+});
